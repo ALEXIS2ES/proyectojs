1bae0043e612d99b436ad5a561ed3167e62bcd52
alexis2es <vccamerccoa@unac.edu.co>
2024-07-23 11:00:14 -0500

    mostrar los productos

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("hola mundo")
+const productos = [
+    {
+        nombre: "hamburguesa simple",
+        costo: 20,
+        codigo: "h100"
+    },
+    {
+        nombre: "hamburguesa doble",
+        costo: 30,
+        codigo: "h200"
+    },
+    {
+        nombre: "papas fritas",
+        costo: 8,
+        codigo: "p100"
+    },
+    {
+        nombre: "refresco personal",
+        costo: 6,
+        codigo: "r100"
+    }
+    ]
+
+const mostrarMenu = () => {
+    console.log(`CODIGO - NOMBRE PRODUCTO - COSTO`)
+    //productos.forEach(producto => console.log(`${producto.codigo} - ${producto.nombre} - ${producto.costo}`))
+
+    for(let producto of productos){
+        console.log(`${producto.codigo} - ${producto.nombre} - ${producto.costo}`)
+    }
+}
+
